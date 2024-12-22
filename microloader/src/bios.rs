e42bd6ca151c28657c8c96f0a92f38076be690aa
javalsai <jvssxxi@gmail.com>
2024-12-22 23:25:04 +0100

    feat: link ptrs into binary

--- microloader/src/bios.rs
+++ microloader/src/bios.rs
@@ -1,44 +1,62 @@
-pub mod calls {
-    pub mod video {
-        // mostly for int 10h, as they seem the most useful for now (display lol)
-        // * https://en.wikipedia.org/wiki/INT_10H
+pub mod video {
+    /// https://en.wikipedia.org/wiki/INT_10H
 
-        use core::arch::asm;
+    use core::arch::asm;
 
-        pub fn set_video_mode(mode: u8) {
-            unsafe {
-                asm!(
-                    "int 0x10",
-                    in("ah") 0x00_u8,
-                    in("al") mode,
-                );
-            }
+    pub fn set_video_mode(mode: u8) {
+        unsafe {
+            asm!(
+                "int 0x10",
+                in("ah") 0x00_u8,
+                in("al") mode,
+            );
         }
+    }
 
-        pub fn set_cursor_pos(page_num: u8, row: u8, col: u8) {
-            unsafe {
-                asm!(
-                    "int 10h",
-                    in("ah") 0x02_u8,
-                    in("bh") page_num,
-                    in("dh") row,
-                    in("dl") col,
-                );
-            }
+    pub fn set_cursor_pos(page_num: u8, row: u8, col: u8) {
+        unsafe {
+            asm!(
+                "int 10h",
+                in("ah") 0x02_u8,
+                in("bh") page_num,
+                in("dh") row,
+                in("dl") col,
+            );
         }
+    }
 
-        // https://en.wikipedia.org/wiki/BIOS_color_attributes
-        pub fn print_char(ch: u8, page_num: u8, color: u8) {
-            unsafe {
-                asm!(
-                    "int 0x10",
-                    in("ah") 0x0e_u8,
-                    in("al") ch,
-                    in("bh") page_num,
-                    in("bl") color,
-                    options(nomem, nostack),
-                );
-            }
+    pub fn print_char(ch: u8, page_num: u8, color: &BiosColor) {
+        unsafe {
+            asm!(
+                "int 0x10",
+                in("ah") 0x0e_u8,
+                in("al") ch,
+                in("bh") page_num,
+                in("bl") *color as u8,
+                options(nomem, nostack),
+            );
         }
+    }
+
+    // https://en.wikipedia.org/wiki/BIOS_color_attributes
+    #[repr(u8)]
+    #[derive(Clone, Copy)]
+    pub enum BiosColor {
+        Black = 0x0,
+        Blue = 0x1,
+        Green = 0x2,
+        Cyan = 0x3,
+        Red = 0x4,
+        Magenta = 0x5,
+        Brown = 0x6,
+        LightGray = 0x7,
+        DarkGray = 0x8,
+        LightBlue = 0x9,
+        LightGreen = 0xA,
+        LightCyan = 0xB,
+        LightRed = 0xC,
+        LightMagenta = 0xD,
+        Yellow = 0xE,
+        White = 0xF,
     }
 }
--- microloader/src/main.rs
+++ microloader/src/main.rs
@@ -16,24 +16,47 @@ pub fn test_runner(_test: &[&i32]) {
 #[lang = "eh_personality"]
 extern "C" fn eh_personality() {}
 
+#[allow(improper_ctypes)]
+unsafe extern "C" {
+    static _stack_start: ();
+    static _stack_end: ();
+    static _mbr_start: ();
+    static _mbr_end: ();
+    static _partition_table: ();
+    static _second_stage_start: ();
+}
+
+macro_rules! get_ld_sym {
+    ($sym:expr) => {
+        unsafe { &$sym as *const () }
+    };
+}
+
+use bios::*;
 use core::{
     panic::PanicInfo,
     sync::atomic::{self, Ordering},
 };
-
-const STRING: &[u8] = b"Hello World!";
+use video::BiosColor;
 
 #[unsafe(no_mangle)]
 fn _start() -> ! {
     // (320x200, 256 colors)
-    bios::calls::video::set_video_mode(0x13);
+    video::set_video_mode(0x13);
 
-    for byte in STRING {
-        bios::calls::video::print_char(*byte, 0, 0xc);
+    for ch in b"ptr of partition table (in decimal):\r\n" {
+        video::print_char(*ch, 0, &BiosColor::LightRed);
     }
+    _dgb_print_u16(get_ld_sym!(_partition_table) as u16, BiosColor::Green);
 
     #[allow(clippy::empty_loop)]
     loop {}
+}
+
+fn _dgb_print_u16(n: u16, c: BiosColor) {
+    for x in [10000, 1000, 100, 10, 1] {
+        video::print_char(b'0' + ((n / x) % 10) as u8, 0, &c);
+    }
 }
 
 #[panic_handler]
